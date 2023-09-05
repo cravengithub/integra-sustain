655ddea7283fefb47ff471da1c1875b3751eaf40
Hastari Utama <utama738@gmail.com>
2023-09-05 15:00:35 +0700

    main

---
+++ main.py
@@ -0,0 +1,40 @@
+class E1:
+    crop = {
+        'size': 0
+    }
+    plant = {
+        'variety': 0,
+        'age': 0
+    }
+    land = None
+    irr_system = {
+        'cost': 0,
+        'technology': None
+    }
+    irr_pipeline = {
+        'covered_area': None
+    }
+    water = 0
+    pass
+
+
+class E2:
+    crop = {
+        'size': 0
+    }
+    plant = {
+        'variety': 0,
+        'annual_prod': 0
+    }
+    land = {
+        'ph_level' : 0
+    }
+    irr_system = {
+        'cost': 0,
+        'technology': None
+    }
+    irr_pipeline = {
+        'covered_area': None
+    }
+    water = 0
+    pass
